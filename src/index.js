2173b4a348461b784a64309236810cf2945205f3
Numan Selman Duman <numanselmanduman@gmail.com>
2023-07-17 13:23:56 +0000

    Create some User and Meeting object

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("Hi coyote, let's have some JavaScript fun!")
+// I need to have two main object, User and Meeting
+// User will have a name and a list of meetings
+// Meeting will have a name, a date, location, description and a list of attendees
+// User should be able to create a meeting
+// User should be able to join a meeting
+// User should be able to leave a meeting
+// User should be able to delete a meeting
+
+const numan = {
+  name: 'Numan',
+  meetings: [],
+}
+
+const mich = {
+  name: 'Mich',
+  meetings: [],
+}
+
+const numansMeeting = {
+  name: 'Numans Meeting',
+  date: '12/12/2023',
+  location: 'Berlin',
+  description: 'This is a meeting for Numan',
+  attendees: [],
+}
