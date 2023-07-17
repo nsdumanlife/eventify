c574dbacec1923a54b70da0c2b76744a41275ce9
Numan Selman Duman <numanselmanduman@gmail.com>
2023-07-17 14:03:58 +0000

    Create User and Meeting classes

--- src/index.js
+++ src/index.js
@@ -6,42 +6,57 @@
 // User should be able to leave a meeting
 // User should be able to delete a meeting
 
-const numan = {
-  name: 'Numan',
-  meetings: [],
+class User {
+  meetings = []
+
+  constructor(name) {
+    this.name = name
+  }
+
+  createMeeting(name, date, location, description) {
+    const newMeeting = new Meeting(name, date, location, description)
+    this.meetings.push(newMeeting)
+    newMeeting.attendees.push(this.name)
+
+    return newMeeting
+  }
+
   joinMeeting(meeting) {
     meeting.attendees.push(this.name)
     this.meetings.push(meeting)
-  },
+  }
+
+  leaveMeeting(meeting) {
+    const indexOfUser = meeting.attendees.indexOf(this.name)
+    meeting.attendees.splice(indexOfUser, 1)
+
+    const indexOfMeeting = this.meetings.indexOf(meeting)
+    this.meetings.splice(indexOfMeeting, 1) // check later?
+  }
 }
 
-const mich = {
-  name: 'Mich',
-  meetings: [],
-  joinMeeting(meeting) {
-    meeting.attendees.push(this.name)
-    this.meetings.push(meeting)
-  },
+class Meeting {
+  attendees = []
+
+  constructor(name, date, location, description) {
+    this.name = name
+    this.date = date
+    this.location = location
+    this.description = description
+  }
 }
 
-const numansMeeting = {
-  name: 'Numans Meeting',
-  date: '12/12/2023',
-  location: 'Berlin',
-  description: 'This is a meeting for Numan',
-  attendees: [],
-}
+const numan = new User('Numan')
+const mich = new User('Mich')
+const marvin = new User('Marvin')
 
-const marvinsMeeting = {
-  name: 'Marvins Meeting',
-  date: '11/11/2023',
-  location: 'Frankurt',
-  description: 'This is a meeting for Marvin',
-  attendees: [],
-}
+const numansMeeting = numan.createMeeting('Numans Meeting', '12/12/2023', 'Berlin', 'This is a meeting for Numan')
+const marvinsMeeting = marvin.createMeeting('Marvins Meeting', '11/11/2023', 'Frankurt', 'This is a meeting for Marvin')
+// const numansMeeting = new Meeting('Numans Meeting', '12/12/2023', 'Berlin', 'This is a meeting for Numan')
+// const marvinsMeeting = new Meeting('Marvins Meeting', '11/11/2023', 'Frankurt', 'This is a meeting for Marvin')
 
 mich.joinMeeting(numansMeeting)
 numan.joinMeeting(marvinsMeeting)
 
 // console.log('mich: ', mich)
-// console.log('numansMeeting: ', numansMeeting)
+console.log('numansMeeting: ', numansMeeting)
