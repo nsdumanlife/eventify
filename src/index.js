2235c10a1c07f78de8559b320137756fc800c3b4
Numan Selman Duman <numanselmanduman@gmail.com>
2023-07-18 13:23:38 +0000

    Add some test cases for instances

--- src/index.js
+++ src/index.js
@@ -51,12 +51,21 @@ const mich = new User('Mich')
 const marvin = new User('Marvin')
 
 const numansMeeting = numan.createMeeting('Numans Meeting', '12/12/2023', 'Berlin', 'This is a meeting for Numan')
-const marvinsMeeting = marvin.createMeeting('Marvins Meeting', '11/11/2023', 'Frankurt', 'This is a meeting for Marvin')
-// const numansMeeting = new Meeting('Numans Meeting', '12/12/2023', 'Berlin', 'This is a meeting for Numan')
-// const marvinsMeeting = new Meeting('Marvins Meeting', '11/11/2023', 'Frankurt', 'This is a meeting for Marvin')
+const marvinsMeeting = marvin.createMeeting(
+  'Marvins Meeting',
+  '11/11/2023',
+  'Frankfurt',
+  'This is a meeting for Marvin'
+)
 
 mich.joinMeeting(numansMeeting)
 numan.joinMeeting(marvinsMeeting)
+numan.leaveMeeting(marvinsMeeting)
 
-// console.log('mich: ', mich)
-console.log('numansMeeting: ', numansMeeting)
+console.log('numan: ', numan)
+// console.log('numansMeeting: ', numansMeeting)
+
+console.log(`User Numan should have name Numan ${numan.name === 'Numan'}`)
+console.log(`User Numan has ${numan.meetings.length} meetings ${numan.meetings.length === 1}`)
+console.log(`Mitch joins Marvins meeting at in Berlin ${numan.meetings[0].location === 'Berlin'}`)
+console.log(`User Marvin has ${marvin.meetings.length} meetings ${marvin.meetings.length === 1}`)
