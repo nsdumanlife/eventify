49fd0341bf9095ad3e6810e6e06e1e17cc067d81
Numan Selman Duman <numanselmanduman@gmail.com>
2023-07-26 13:00:20 +0000

    Add user creation route handler

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+const User = require('../user')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource')
+})
 
-module.exports = router;
+router.post('/', async function (req, res, next) {
+  const user = User.create({ name: req.body.name })
+  res.send(user)
+})
+
+module.exports = router
