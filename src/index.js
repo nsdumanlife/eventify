b75365891abb40d141a503180c479d53e7f42c1d
Numan Selman Duman <numanselmanduman@gmail.com>
2023-07-28 13:31:52 +0000

    Update index.js to test users API

--- src/index.js
+++ src/index.js
@@ -5,41 +5,55 @@
 // User should be able to join a meeting
 // User should be able to leave a meeting
 // User should be able to delete a meeting
+const axios = require('axios')
 const User = require('./user')
 const chalk = require('chalk')
 
-const numan = new User('Numan')
-const mich = new User('Mich')
-const marvin = new User('Marvin')
+async function main() {
+  const numan = await axios.post('http://localhost:3000/users', { name: 'Numan' })
+  console.log('numan: ', numan.data)
 
-const numansMeeting = numan.createMeeting('Numans Meeting', '12/12/2023', 'Berlin', 'This is a meeting for Numan')
-const marvinsMeeting = marvin.createMeeting(
-  'Marvins Meeting',
-  '11/11/2023',
-  'Frankfurt',
-  'This is a meeting for Marvin'
-)
+  const updatedNuman = await axios.put('http://localhost:3000/users/Numan', { name: 'Selman' })
+  console.log('updatedNuman: ', updatedNuman.data)
 
-mich.joinMeeting(numansMeeting)
-numan.joinMeeting(marvinsMeeting)
-numan.leaveMeeting(marvinsMeeting)
+  const allUsers = await axios.get('http://localhost:3000/users')
 
-// console.log('numan: ', numan)
-// console.log('numansMeeting: ', numansMeeting)
+  console.log('all users: ', allUsers.data)
+}
+main()
 
-console.log(`User Numan should have name Numan ${numan.name === 'Numan' ? chalk.blue(true) : chalk.red(false)}`)
-console.log(
-  `User Numan has ${numan.meetings.length} meetings ${
-    numan.meetings.length === 1 ? chalk.blue(true) : chalk.red(false)
-  }`
-)
-console.log(
-  `Mitch joins Marvins meeting at in Berlin ${
-    numan.meetings[0].location === 'Berlin' ? chalk.blue(true) : chalk.red(false)
-  }`
-)
-console.log(
-  `User Marvin has ${marvin.meetings.length} meetings ${
-    marvin.meetings.length === 1 ? chalk.blue(true) : chalk.red(false)
-  }`
-)
+// const numan = new User('Numan')
+// const mich = new User('Mich')
+// const marvin = new User('Marvin')
+
+// const numansMeeting = numan.createMeeting('Numans Meeting', '12/12/2023', 'Berlin', 'This is a meeting for Numan')
+// const marvinsMeeting = marvin.createMeeting(
+//   'Marvins Meeting',
+//   '11/11/2023',
+//   'Frankfurt',
+//   'This is a meeting for Marvin'
+// )
+
+// mich.joinMeeting(numansMeeting)
+// numan.joinMeeting(marvinsMeeting)
+// numan.leaveMeeting(marvinsMeeting)
+
+// // console.log('numan: ', numan)
+// // console.log('numansMeeting: ', numansMeeting)
+
+// console.log(`User Numan should have name Numan ${numan.name === 'Numan' ? chalk.blue(true) : chalk.red(false)}`)
+// console.log(
+//   `User Numan has ${numan.meetings.length} meetings ${
+//     numan.meetings.length === 1 ? chalk.blue(true) : chalk.red(false)
+//   }`
+// )
+// console.log(
+//   `Mitch joins Marvins meeting at in Berlin ${
+//     numan.meetings[0].location === 'Berlin' ? chalk.blue(true) : chalk.red(false)
+//   }`
+// )
+// console.log(
+//   `User Marvin has ${marvin.meetings.length} meetings ${
+//     marvin.meetings.length === 1 ? chalk.blue(true) : chalk.red(false)
+//   }`
+// )
