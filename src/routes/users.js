7922eb559f1734ef1e2d5f16df13369d9e5c4cf2
Numan Selman Duman <numanselmanduman@gmail.com>
2023-07-28 13:31:00 +0000

    Add update user route handler

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,9 +7,21 @@ router.get('/', function (req, res, next) {
   res.send(User.list)
 })
 
+// Create a new user
 router.post('/', async function (req, res, next) {
   const user = User.create({ name: req.body.name })
   res.send(user)
+})
+
+// Update a user
+router.put('/:id', async function (req, res, next) {
+  try {
+    const user = User.list.find(user => user.name === req.params.id)
+    user.name = req.body.name
+    res.send(user)
+  } catch (error) {
+    res.status(404).send('User not found')
+  }
 })
 
 module.exports = router
