6a081613c6ea791750fbd130474dc1d1aa2ff57f
Numan Selman Duman <numanselmanduman@gmail.com>
2023-08-01 15:50:37 +0000

    Update file paths after moving classes to models folder

--- src/routes/meetings.js
+++ src/routes/meetings.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
-const Meeting = require('../meeting')
-const User = require('../user')
+const Meeting = require('../models/meeting')
+const User = require('../models/user')
 
 // get all meetings
 router.get('/', function (req, res, next) {
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const User = require('../user')
+const User = require('../models/user')
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
