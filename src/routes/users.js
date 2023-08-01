e680f459c574f2bd415a7383351fe3d6bddab98a
Numan Selman Duman <numanselmanduman@gmail.com>
2023-08-01 17:56:34 +0000

    Update routes after implementing mongoose schemas

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,13 +3,15 @@ const router = express.Router()
 const User = require('../models/user')
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send(User.list)
+router.get('/', async function (req, res, next) {
+  const users = await User.find()
+
+  res.send(users)
 })
 
 // Get user details by id
-router.get('/:id', function (req, res, next) {
-  const user = User.list.find(user => user.name === req.params.id)
+router.get('/:id', async function (req, res, next) {
+  const user = await User.findById(req.params.id)
   res.send(user)
 })
 
@@ -22,8 +24,8 @@ router.post('/', async function (req, res, next) {
 // Update a user
 router.put('/:id', async function (req, res, next) {
   try {
-    const user = User.list.find(user => user.name === req.params.id)
-    user.name = req.body.name
+    const user = await User.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true })
+    // user.name = req.body.name
     res.send(user)
   } catch (error) {
     res.status(404).send('User not found')
