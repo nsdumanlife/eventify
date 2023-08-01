7695d3579149e14e794942e9d94199f024a282ae
Numan Selman Duman <numanselmanduman@gmail.com>
2023-08-01 13:36:26 +0000

    Add static create() and list to meeting class

--- src/meeting.js
+++ src/meeting.js
@@ -7,6 +7,14 @@ class Meeting {
     this.location = location
     this.description = description
   }
+
+  static create({ name, date, location, description }) {
+    const meeting = new Meeting(name, date, location, description)
+    Meeting.list.push(meeting)
+    return meeting
+  }
+
+  static list = []
 }
 
 module.exports = Meeting
