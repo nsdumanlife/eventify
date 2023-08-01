3a90c6ae0292f170b7331279c539c18becc75e9f
Numan Selman Duman <numanselmanduman@gmail.com>
2023-08-01 13:34:05 +0000

    Add meetings router to app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const logger = require('morgan')
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
+const meetingsRouter = require('./routes/meetings')
 
 const app = express()
 
@@ -19,8 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+app.use('/meetings', meetingsRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
