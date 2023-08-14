9a6d666d15fcbd15fef541d954902eafc386fafe
Numan Selman Duman <numanselmanduman@gmail.com>
2023-08-14 15:33:45 +0000

    Create CounterOptions cmp

---
+++ frontend/src/components/CounterOptions.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: 'CounterOptions',
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    },
+    decrement() {
+      this.count--
+    }
+  }
+}
+</script>
+
+<template lang="pug">
+div
+  h1 Counter
+  p {{ count }}
+  button(@click="increment") +
+  button(@click="decrement") -
+</template>
+
+<style lang="scss" scoped></style>
