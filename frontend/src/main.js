c9e664e9f24e5ec7fe59e18c8d75ffd10ab333c2
Numan Selman Duman <numanselmanduman@gmail.com>
2023-08-15 13:51:22 +0000

    Add bootstrap to frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,9 @@
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/main.css'
 
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'
