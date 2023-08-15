9a663ecc640afa3e77f9546f106e52b3ed7ab87e
Numan Selman Duman <numanselmanduman@gmail.com>
2023-08-15 09:20:10 +0000

    Create MeetingsView

---
+++ frontend/src/views/MeetingsView.vue
@@ -0,0 +1,29 @@
+<script>
+import { mapActions } from 'pinia'
+import { useMeetingStore } from '../stores/meeting.js'
+
+export default {
+  name: 'MeetingsView',
+  data() {
+    return {
+      meetings: []
+    }
+  },
+  async created() {
+    this.meetings = await this.fetchMeetings()
+  },
+  methods: {
+    ...mapActions(useMeetingStore, ['fetchMeetings'])
+  }
+}
+</script>
+
+<template lang="pug">
+div
+  h1 Meetings
+  ul
+    li(v-for='meeting in meetings')
+      p {{meeting.name}}
+</template>
+
+<style scoped></style>
