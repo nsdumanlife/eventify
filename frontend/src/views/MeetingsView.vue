7306d77dde010daf90740dbdc7fbd7716e8c7d3a
Numan Selman Duman <numanselmanduman@gmail.com>
2023-08-18 14:08:54 +0000

    Add some style and can view meeting details by clicking view button

--- frontend/src/views/MeetingsView.vue
+++ frontend/src/views/MeetingsView.vue
@@ -1,6 +1,7 @@
 <script>
 import { mapActions } from 'pinia'
 import { useMeetingStore } from '../stores/meeting.js'
+import { RouterLink } from 'vue-router'
 
 export default {
   name: 'MeetingsView',
@@ -23,7 +24,22 @@ div
   h1 Meetings
   ul
     li(v-for='meeting in meetings')
-      p {{meeting.name}}
+      .card
+        .card-body
+          h5.card-title {{meeting.name}}
+          p.card-text {{meeting.location}}
+          p.card-text {{meeting.date}}
+          RouterLink.btn.btn-primary(:to="`/meetings/${meeting._id}`") View
+
 </template>
 
-<style scoped></style>
+<style scoped>
+.card {
+  margin-bottom: 1rem;
+  width: 18rem;
+}
+
+li {
+  list-style: none;
+}
+</style>
