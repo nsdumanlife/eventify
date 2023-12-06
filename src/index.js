60d15f73dffcd094becd95fb6477e0dad8d339ea
Numan Selman Duman <numanselmanduman@gmail.com>
2023-12-06 14:21:52 +0000

    Create some example objects and test logs

--- src/index.js
+++ src/index.js
@@ -4,22 +4,48 @@
 // User should be able to create a meeting
 // User should be able to join a meeting
 // User should be able to leave a meeting
-// User should be able to delete a meeting
 
 const numan = {
-  name: 'Numan',
+  name: 'numan',
+  age: 30,
   meetings: [],
+  joinMeeting(meeting) {
+    meeting.attendees.push(this)
+    this.meetings.push(meeting)
+  },
 }
 
-const mich = {
-  name: 'Mich',
+const ege = {
+  name: 'ege',
+  age: '20',
   meetings: [],
+  joinMeeting(meeting) {
+    meeting.attendees.push(this)
+    this.meetings.push(meeting)
+  },
 }
 
-const numansMeeting = {
-  name: 'Numans Meeting',
-  date: '12/12/2023',
-  location: 'Berlin',
-  description: 'This is a meeting for Numan',
+const egesMeeting = {
+  name: 'eges meeting',
+  location: 'Istanbul',
+  date: '12.12.2023',
+  description: 'Meeting for JS101 lecture',
   attendees: [],
 }
+
+const preetsBdayParty = {
+  name: 'Preets Bday Party',
+  location: 'Hamburg',
+  date: '10.12.2023',
+  description: 'You are all welcome',
+  attendees: [],
+}
+
+numan.joinMeeting(egesMeeting)
+ege.joinMeeting(preetsBdayParty)
+numan.joinMeeting(preetsBdayParty)
+
+console.log('length of numans meetings: ', numan.meetings.length)
+console.log('numan is attending 2 meetings : ', numan.meetings.length === 2)
+console.log('Number of attendees of eges meeting should be 1 :', egesMeeting.attendees.length === 1)
+console.log('Number of attendees of preetsBdayParty meeting should be 2 :', preetsBdayParty.attendees.length === 2)
