7aa036ea3eaed7256fb750e74a9c361906c4c24f
Numan Selman Duman <numanselmanduman@gmail.com>
2023-12-15 14:20:06 +0000

    Implent express.js and rebuild project folder structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
-// I need to have two main object, User and Meeting
-// User will have a name and a list of meetings
-// Meeting will have a name, a date, location, description and a list of attendees
-// User should be able to create a meeting
-// User should be able to join a meeting
-// User should be able to leave a meeting
-
 const User = require('./user')
-const Meeting = require('./meeting')
 
 const numan = new User('numan', 30)
 const ege = new User('Ege', 18)
@@ -26,12 +18,12 @@ selman.joinMeeting(preetsBdayParty)
 
 numan.leaveMeeting(preetsBdayParty)
 ege.leaveMeeting(preetsBdayParty)
-// selman.leaveMeeting(preetsBdayParty)
+selman.leaveMeeting(preetsBdayParty)
 
 // console.log('age of numan: ', numan.age)
-// console.log('length of numans meetings: ', numan.meetings.length)
-// console.log('numan is attending 2 meetings : ', numan.meetings.length === 2)
-// console.log('Number of attendees of eges meeting should be 1 :', egesMeeting.attendees.length === 1)
-// console.log('Number of attendees of preetsBdayParty meeting should be 2 :', preetsBdayParty.attendees.length === 2)
+console.log('length of numans meetings: ', numan.meetings.length)
+console.log('numan is attending 1 meetings : ', numan.meetings.length === 1)
+console.log('Number of attendees of eges meeting should be 2 :', egesMeeting.attendees.length === 2)
+console.log('Number of attendees of preetsBdayParty meeting should be 2 :', preetsBdayParty.attendees.length === 2)
 console.log('Preets Bday Party : ', preetsBdayParty)
 console.log('numan: ', numan)
