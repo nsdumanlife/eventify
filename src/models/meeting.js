e22d16ecd01e72494e030f84a9c8f047077cc41b
Numan Selman Duman <numanselmanduman@gmail.com>
2024-01-08 12:40:42 +0000

    Add mongoose-autopopulate to meeting model for attendees details

--- src/models/meeting.js
+++ src/models/meeting.js
@@ -1,12 +1,24 @@
 const mongoose = require('mongoose')
+const autopopulate = require('mongoose-autopopulate')
 
 const meetingSchema = new mongoose.Schema({
   name: String,
   location: String,
   date: String,
   description: String,
-  attendees: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  attendees: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      autopopulate: {
+        maxDepth: 1,
+      },
+    },
+  ],
 })
 class Meeting {}
+
+meetingSchema.loadClass(Meeting)
+meetingSchema.plugin(autopopulate)
 
 module.exports = mongoose.model('Meeting', meetingSchema)
