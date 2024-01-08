6748f064727c0ac7a60bae0355be67e75e68182f
Numan Selman Duman <numanselmanduman@gmail.com>
2024-01-08 12:18:43 +0000

    Add delete /db API to clear the database
    Created a new API endpoint '/db' that uses the DELETE method to delete all records from the 'User' and 'Meeting' collections. This ensures a clean slate to prevent duplicate users and meetings.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express')
 var router = express.Router()
 
+const User = require('../models/user')
+const Meeting = require('../models/meeting')
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Meeting App' })
+})
+
+router.delete('/db', async function (req, res, next) {
+  await User.deleteMany({})
+  await Meeting.deleteMany({})
+  res.sendStatus(200)
 })
 
 module.exports = router
