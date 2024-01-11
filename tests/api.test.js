7f29799b9d545e250cf630a6e8303255e1f05162
Numan Selman Duman <numanselmanduman@gmail.com>
2024-01-11 17:37:01 +0000

    Add API test for creating a user

---
+++ tests/api.test.js
@@ -0,0 +1,23 @@
+const request = require('supertest')
+const app = require('../src/app')
+
+describe('API tests', () => {
+
+  it('should create a user', async () => {
+    const name = 'numan'
+    const age = 30
+    const meetings = []
+
+    const expectedOutput = {
+      name,
+      age,
+      meetings,
+    }
+
+    const actualOutput = await request(app).post('/users').send({ name, age })
+
+    expect(actualOutput.statusCode).toEqual(200)
+    expect(actualOutput.body._id).toBeDefined()
+    expect(actualOutput.body).toMatchObject(expectedOutput)
+  })
+})
