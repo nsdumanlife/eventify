7feb49a24007d144059e673788888ac7c4b7d655
Numan Selman Duman <numanselmanduman@gmail.com>
2024-01-16 19:48:16 +0000

    Add axios dependency and create MeetingsView component

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -16,6 +16,22 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/meetings',
+      name: 'meetings',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/MeetingsView.vue')
+    },
+    {
+      path: '/meeting/:id',
+      name: 'meeting',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/MeetingDetailView.vue')
     }
   ]
 })
---
+++ frontend/src/views/MeetingsView.vue
@@ -0,0 +1,37 @@
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'MeetingsView',
+  components: {},
+  data() {
+    return {
+      meetings: []
+    }
+  },
+  computed: {},
+  methods: {
+    async fetchMeetings() {
+      this.meetings = (await axios.get('http://localhost:3000/meetings')).data
+    }
+  },
+  mounted() {
+    this.fetchMeetings()
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>Meetings</h1>
+    <ul>
+      <li v-for="meeting in meetings" :key="meeting._id">
+        <router-link :to="{ name: 'meeting', params: { id: meeting._id } }">
+          {{ meeting.name }}
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
